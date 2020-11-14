f16ae7ff9bf01d9ac8e242b32e49fdf50052beb2
Russkazkin <ruslan@skazkin.su>
2020-11-14 15:35:10 +0300

    Generate SettingsTest

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
     public function index()
     {
+        $settings = new Setting();
+        dd($settings->admin);
         return view('setting.index');
     }
 }
