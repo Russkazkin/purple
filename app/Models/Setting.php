d0d47516f1889a590defb51951d1e043df3727c2
Russkazkin <ruslan@skazkin.su>
2020-11-14 19:48:35 +0300

    Move default settings into json file and add accessor

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -6,17 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
+    private string $default;
+
     protected $casts = [
         'public' => 'array',
         'admin' => 'array',
     ];
 
-    protected $attributes = [
-        'admin' => '{
-            "notifications": {
-                "webpushes": "1",
-                "toastr": "1"
-            }
-        }'
-    ];
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->default = file_get_contents(config_path('settings/admin.json'));
+    }
+
+    public function getAdminAttribute()
+    {
+        return $this->castAttribute('admin', $this->default);
+    }
 }
+
+
