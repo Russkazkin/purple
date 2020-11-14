4784d1bf9ac6c833db08806090751684e13448dc
Russkazkin <ruslan@skazkin.su>
2020-11-14 16:08:41 +0300

    Add users_cant_get_admin_settings test

--- tests/Feature/Admin/User/SettingsTest.php
+++ tests/Feature/Admin/User/SettingsTest.php
@@ -1,9 +1,17 @@
 <?php
 
-test('guests_cant_get_settings', function () {
+use App\Models\User;
+
+test('guests_cant_get_admin_settings', function () {
     /* @var \Tests\TestCase $this */
 
-    $this->withExceptionHandling();
-
     $this->get('/api/admin/settings', ['Accept' => 'application/json'])->assertStatus(401);
+});
+
+test('users_cant_get_admin_settings', function () {
+    /* @var \Tests\TestCase $this */
+
+    $this->actingAs(factory(User::class)->create(), 'api');
+
+    $this->get('/api/admin/settings')->assertForbidden();
 });
