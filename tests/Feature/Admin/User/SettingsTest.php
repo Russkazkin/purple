dd5a6495461ef118f01fa2b677b401167930c2a2
Russkazkin <ruslan@skazkin.su>
2020-11-14 16:19:21 +0300

    Setup admins settings tests

--- tests/Feature/Admin/User/SettingsTest.php
+++ tests/Feature/Admin/User/SettingsTest.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+beforeEach(function () {
+    $this->app->make(PermissionRegistrar::class)->registerPermissions();
+
+    Role::create(['name' => 'admin']);
+    Role::create(['name' => 'super-admin']);
+});
 
 test('guests_cant_get_admin_settings', function () {
     /* @var \Tests\TestCase $this */
@@ -14,4 +23,16 @@ test('users_cant_get_admin_settings', function () {
     $this->actingAs(factory(User::class)->create(), 'api');
 
     $this->get('/api/admin/settings')->assertForbidden();
+});
+
+test('admins_can_get_default_admin_settings', function () {
+    /* @var \Tests\TestCase $this */
+
+    $admin = factory($user = User::class)->create();
+
+    $admin->assignRole('admin');
+
+    $this->actingAs($admin, 'api');
+
+    $this->get('/api/admin/settings')->assertOk();
 });
