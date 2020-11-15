2ce2e3ddccec2361b7929f340f134976f610b754
Russkazkin <ruslan@skazkin.su>
2020-11-15 08:12:00 +0300

    Move default settings  getting from contstractor to accessor

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,26 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\Yaml\Yaml;
 
 class Setting extends Model
 {
     private string $default;
-
     protected $casts = [
         'public' => 'array',
         'admin' => 'array',
     ];
 
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
-
-        $this->default = file_get_contents(config_path('settings/admin.json'));
-    }
-
     public function getAdminAttribute()
     {
-        return $this->castAttribute('admin', $this->default);
+        return Yaml::parseFile(config_path('settings/admin.yaml'));
     }
 }
 
